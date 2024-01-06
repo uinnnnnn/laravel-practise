b56437888dc21a270249f9593a68ff2a40d97a61
林映如 <sumi143yy@gmail.com>
2024-01-07 04:36:55 +0800

    demo_email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,35 +1,27 @@
 <?php
-
+  
 namespace App\Http\Controllers;
-
+  
 use Illuminate\Http\Request;
 use Mail;
 use App\Mail\DemoMail;
-
+  
 class MailController extends Controller
 {
-    // /**
-    //  * Write code on Method
-    //  *
-    //  * @return response()
-    //  */
-    // public function index()
-    // {
-    //     $mailData = [
-    //         'title' => 'Mail from ItSolutionStuff.com',
-    //         'body' => 'This is for testing email using smtp.'
-    //     ];
-
-    //     Mail::to('sumi143yy@gmail.com')->send(new DemoMail($mailData));
-
-    //     dd("Email is sent successfully.");
-    // }
-    public function send()
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index()
     {
-        request()->validate(['email' => 'required|email']);
-
-        Mail::to(request('email'))->send(new DemoMail('Amy Chen')); //主要跟上次差別在這行
-
-        return redirect('/')->with('message', 'email sent');
+        $mailData = [
+            'title' => 'Mail from ItSolutionStuff.com',
+            'body' => 'This is for testing email using smtp.'
+        ];
+         
+        Mail::to('sumi143yy@gmail.com')->send(new DemoMail($mailData));
+           
+        dd("Email is sent successfully.");
     }
 }
--- app/Mail/DemoMail.php
+++ app/Mail/DemoMail.php
@@ -1,46 +1,36 @@
 <?php
-  
+
 namespace App\Mail;
-  
+
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-  
+
 class DemoMail extends Mailable
 {
     use Queueable, SerializesModels;
-  
-    public $mailData;
-    public $name;
-  
-    // /**
-    //  * Create a new message instance.
-    //  *
-    //  * @return void
-    //  */
-    // public function __construct($mailData)
-    // {
-    //     $this->mailData = $mailData;
-    // }
-  
-    // /**
-    //  * Build the message.
-    //  *
-    //  * @return $this
-    //  */
-    // public function build()
-    // {
-    //     return $this->subject('Mail from ItSolutionStuff.com')
-    //                 ->view('emails.demoMail');
-    // }
-    public function __construct(string $name)
-{
-    $this->name = $name; //可以傳到html的view檔案做使用
-    $this->subject = 'Notification to '.$name; //信件標題
-}
 
-public function build(){
-		return view('example_mail'); //引用html的view檔案
-}
+    public $mailData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Mail from ItSolutionStuff.com')
+            ->view('emails.demoMail');
+    }
 }
